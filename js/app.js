4b3f666af89d5ae2c2e28d80200659aad6d35b68
Nicollas Feitosa <contatonicollasfeitosa@gmail.com>
2020-09-29 11:59:38 -0300

    Post Launch Update #2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-$("#cnpj").blur(function (e) {
+$(document).ready(function () { 
+  jQuery.validator.setDefaults({
+    errorPlacement: function(error, element) {
+        if ($('#cnpj-error').length) {
+          return false
+        }
+        error.appendTo('#validation_error_message');
+    }
+  });
 
+  $('#cep').mask('00000-000', {reverse: false});
+  $('#cnpj').mask('00.000.000/0000-00', {reverse: false});
+
+  $('form').validate({
+    rules: {
+        cnpj: {
+            cnpjBR: true // Método do additional-methods
+        },
+        email: {
+            email: true // Método do additional-methods
+        }
+    }
+  })
+});
+
+$("#cnpj").blur(function (e) {
     var cnpj = $("#cnpj").val() || ''
 
     cnpj = cnpj.replace('/', '')
@@ -12,12 +36,14 @@ $("#cnpj").blur(function (e) {
         return
     }
 
-    buscaCNPJ(cnpj);
+    if ($('form').valid()) {
+      buscaCNPJ(cnpj)
+      $('#validation_error_message').html('')
+    }
 
 })
 
 function buscaCNPJ(cnpj) {
-
     if (!cnpj) {
         return
     }
@@ -28,16 +54,16 @@ function buscaCNPJ(cnpj) {
         url: url,
         dataType: "jsonp",
         type: "GET",
-        jsonpCallback: 'processJSONPResponse', // add this property
+        jsonpCallback: 'processJSONPResponse',
         contentType: "application/json; charset=utf-8",
         success: function(result, status, xhr) {
-          console.log(result);
+          //console.log(result); // Debug
           preencheCampos(result);
         },
         error: function(xhr, status, error) {
           console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
         }
-      });
+    });
 }
 
 function preencheCampos(dados) {
@@ -56,19 +82,14 @@ function preencheCampos(dados) {
 }
 
 function copiar(input){
-  
   var copyText = document.getElementById(input);
-
   copyText.select();
   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
   document.execCommand("copy");
 }
 
 function copyJson(){
-
     var text = JSON.stringify($('form').serializeArray());
-    
     $("#copyJson").show().val(text)[0].select();
     document.execCommand("copy");
     $("#copyJson").hide();
